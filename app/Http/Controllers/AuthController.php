fd15c7dc6bbddabdc274740051e946eeaf316dc3
manzaero <ligoero@gmail.com>
2024-05-20 22:26:51 +0300

    creating authController & write new params in route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'gender' => 'required|string'
+        ]);
+        if ($validator->fails()){
+            return response() -> json($validator->errors(), 400);
+        }
+        $user = User::create([
+           'email' => $request->email,
+           'password' => Hash::make($request->password),
+           'gender' => $request->gender,
+        ]);
+        $token = JWTAuth::fromUser($user);
+        return response()->json(compact('user', 'token'),201);
+    }
+
+    public function profile(){
+        $user = auth()->user();
+        return response()->json(compact('user'));
+    }
 }
